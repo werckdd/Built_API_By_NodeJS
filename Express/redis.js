2eb2e4e9deb87b6b58edd2ba04d5b909a66a6972
werckdd <lamerialChan@gmail.com>
2017-09-26 21:04:16 +0800

    redis vote

--- Express/redis.js
+++ Express/redis.js
@@ -19,7 +19,9 @@ client.hmset('article:1', {
 client.set('article:',3)
 client.zadd('time:', [1332065417.47, 'article:3', 1332075503.49, 'article:2', 1331382699.33,'article:1'])
 client.zadd('score:', [1332174713.47, 'article:3', 1332225027.26, 'article:2',1332164163.49, 'article:1'])
-client.sadd('voted:1',['user:1','user:2','user:3','user:4','user:5'])
+client.sadd('voted:1', ['user:1', 'user:2', 'user:3', 'user:4', 'user:5'])
+//group
+client.sadd('groups:programming',['article:1','article:3'])
 
 //redis不支持回滚，事务失败时不进行回滚，而是继续执行余下的命令，所以等于不支持事务性
 client.sadd('voted:1', 'user:8', (error, result) => {
@@ -34,8 +36,40 @@ client.sadd('voted:1', 'user:8', (error, result) => {
     }
 })
 
-client.hgetall('article:1').then(result => {
+const get_group_articles = async (conn, group, order = 'score:') => {
+    let key = `order:${group}`
+    
+    const result = await conn.exists(key)
+    //判断是否已经缓存排序结果，如果没有就现在排序
+    if (!result) {
+        const t = await conn.zinterstore(key, 2, [`groups:${group}`, order])
+    }
+    //为了减少redis的工作量，将排序后的集合缓存60s
+    await conn.expire(key, 60)
+
+    return get_articles(conn, key)
+    
+}
+const get_articles = async (conn, order = 'score:') => {
+
+    let ids = []
+    let articles = []
+    let article_data = {}
+
+    const result = await conn.zrevrange(order, 0, -1)
+    ids = result
+    for (let id of ids) {
+        const replies = await client.hgetall(id)
+        article_data[id] = replies
+    }
+
+    return article_data
+
+}
+
+get_group_articles(client, 'programming', 'score:').then(result => {
     console.log(result)
 })
+
 
 export default client
--- Express/resolvers.js
+++ Express/resolvers.js
@@ -40,22 +40,19 @@ const ONE_WEEK_IN_SECONDS = 7 * 86400
 const VOTE_SCORE = 432
 const ARTICLE_PER_PAGE=25
 
-const post_article = (conn, user, title, link) => {
-    let article_id =0
-    conn.incr('article:', (err, result) => {
-        article_id = result
-    })
+const post_article = async (conn, user, title, link) => {
+    const article_id = await conn.incr('article:')
 
     let voted = 'voted:' + article_id
-    conn.sadd(voted, user)
+    await conn.sadd(voted, user)
     //给已经投票用户集合设置过期时间
-    conn.expire(voted, ONE_WEEK_IN_SECONDS)
+    await conn.expire(voted, ONE_WEEK_IN_SECONDS)
     
     //添加文章
     let article = 'article:' + article_id
     //发布时间
     let now = Date.now()
-    conn.hmset(article, {
+    await conn.hmset(article, {
         title: title,
         link: link,
         poster: user,
@@ -63,59 +60,84 @@ const post_article = (conn, user, title, link) => {
         votes:1
     })
 
-    conn.zadd('time:', now, article)
-    conn.zadd('score:', 1 * VOTE_SCORE + now, article)
+    await conn.zadd('time:', now, article)
+    await conn.zadd('score:', 1 * VOTE_SCORE + now, article)
     
     return article_id
 }
 
-const article_vote = (conn, user, article) => {
+const article_vote = async (conn, user, article,approval=true) => {
 
     let cutoff = Date.now() - ONE_WEEK_IN_SECONDS
     if (conn.zscore('time:', article) < cutoff) return false
     
     let article_id = article.split(':')[1]
-    //tranction
-    conn.multi()
-        .sadd(`voted:${ article_id }`, user)
-        .zincrby('score:', VOTE_SCORE ,article)
-        .hincrby(article, 'votes', 1)
-        .exec((error, replies) => {
-            console.log(replies.slice(-1)[0][1])
-        })
-      
+    //redis不支持回滚，事务失败时不进行回滚，而是继续执行余下的命令，所以等于不支持事务性
+    if (approval) {
+        await conn.multi()
+            .sadd(`voted:${article_id}`, user)
+            .zincrby('score:', VOTE_SCORE, article)
+            .hincrby(article, 'votes', 1)
+            .exec((error, replies) => {
+                console.log(replies.slice(-1)[0][1])
+            })
+    } else {
+        await conn.multi()
+            .sadd(`voted:${article_id}`, user)
+            .zdecrby('score:', VOTE_SCORE, article)
+            .hdecrby(article, 'votes', 1)
+            .exec((error, replies) => {
+                console.log(replies.slice(-1)[0][1])
+            }) 
+    }
+   
+        
 }
 
 //获取第几页评分最高的文章和最新发布的文章
-const get_articles = (conn, page, order = 'score') => {
+const get_articles = async (conn, page, order = 'score:') => {
     //起始索引和结束索引
     let start = (page - 1) * ARTICLE_PER_PAGE
     let end = start + ARTICLE_PER_PAGE - 1
     
-    const ids = []
-        articles = []
-        article_data = {}
-    conn.zrevrange(order, start, end).then(result => {
-        ids=result
-    })
+    let ids = []
+    let articles = []
+    let article_data = {}
+ 
+    const result = await conn.zrevrange(order, start,end )
+    ids = result
     for (let id of ids) {
-        client.hgetall(id).then(result => {
-            article_data[id] = result
-        })
+        const replies = await client.hgetall(id)
+        article_data[id] = replies
     }
+
     return article_data
 }
 
 //分组
-const add_remove_groups = (conn, article_id, to_add = [], to_remove = [])=>{
+const add_remove_groups = async (conn, article_id, to_add = [], to_remove = [])=>{
     article = 'article' + article_id
     
     for (let group of to_add) {
-        conn.sadd('group'+group,article)
+        await conn.sadd('group'+group,article)
     }
     for (let group of to_remove) {
-        conn.srem('group'+group,article)
+        await conn.srem('group'+group,article)
     }
+}
+
+const get_group_articles = async (conn, group, page, order = 'score:') => {
+    let key = `order${group}`
+
+    const result = await conn.exists(key)
+    //判断是否已经缓存排序结果，如果没有就现在排序
+    if (!result) {
+        const t = await conn.zinterstore(key, 2, [`groups:${group}`, order])
+    }
+    //为了减少redis的工作量，将排序后的集合缓存60s
+    await conn.expire(key, 60)
+
+    return get_articles(conn, key)
 }
 
 export default resolvers
